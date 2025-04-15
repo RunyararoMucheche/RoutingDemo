289c0ed15ab24d8d00099cd8cc2b4138b2219085
RunyararoMucheche <your_email@example.com>
2025-04-15 13:18:58 -0400

    Add admin route and edit index.js file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const shelterRoutes = require("./routes/shelter");
+const dogRoutes = require("./routes/dogs");
+const adminRoutes = require("./routes/admin");
 
 app.use("/shelters", shelterRoutes);
+app.use("/dogs", dogRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(3060, () => {
   console.log("LISTENING ON PORT 3060");
